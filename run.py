325ed203f99beae7f122807a8cb3ad3e58e94c39
Nexa server <nexaserver@hackheim.no>
2018-08-19 19:32:36 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import json
+import paho.mqtt.publish as publish
+import tellcore.telldus as td
+
+with open('settings.json') as data_file:    
+    settings = json.load(data_file)
+
+def raw_event(data, controller_id, cid):
+    string = "[RAW] {0} <- {1}".format(controller_id, data)
+    values = dict(s.split(':') for s in data.split(';')[:-1])
+    auth = {'username':settings["username"], 'password':settings["password"]} 
+    publish.single(settings["prefix"]+"/"+values['house']+"/"+values['unit'], values['method'], hostname=settings["hostname"], auth = auth)
+
+
+try:
+    import asyncio
+    loop = asyncio.get_event_loop()
+    dispatcher = td.AsyncioCallbackDispatcher(loop)
+except ImportError:
+    loop = None
+    dispatcher = td.QueuedCallbackDispatcher()
+
+core = td.TelldusCore(callback_dispatcher=dispatcher)
+callbacks = []
+
+callbacks.append(core.register_raw_device_event(raw_event))
+
+
+try:
+    if loop:
+        loop.run_forever()
+    else:
+        import time
+        while True:
+            core.callback_dispatcher.process_pending_callbacks()
+            time.sleep(0.5)
+except KeyboardInterrupt:
+    pass
